daf2f2e04d45f9c92beb2c26eb3fc4caff17005c
6arjis <barjjishasanchowdhury96@gmail.com>
2025-03-12 02:08:02 +0600

    Eleventh Commit

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+import Loading from "../Components/Loading";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+
   return (
     <div>
+      {navigation.state === "loading" && <Loading></Loading>}
       <header>
         <Header></Header>
       </header>
